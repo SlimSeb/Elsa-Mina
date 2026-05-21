6ebeb01926645e1d571be4c808cb84a74ce06a3e
slimf1 <slimane.fakani.dev@gmail.com>
2026-05-21 23:22:44 +0200

    test(commands): update tests for btc command

--- test/ElsaMina.UnitTests/Commands/Misc/BitcoinCommandTest.cs
+++ test/ElsaMina.UnitTests/Commands/Misc/BitcoinCommandTest.cs
@@ -1,11 +1,13 @@
 using ElsaMina.Commands.Misc;
 using ElsaMina.Core.Contexts;
 using ElsaMina.Core.Services.Http;
+using ElsaMina.Core.Services.Rooms;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
 
 namespace ElsaMina.UnitTests.Commands.Misc;
 
+[TestFixture]
 public class BitcoinCommandTests
 {
     private IHttpService _httpService;
@@ -17,48 +19,53 @@ public class BitcoinCommandTests
     {
         _httpService = Substitute.For<IHttpService>();
         _context = Substitute.For<IContext>();
-
         _bitcoinCommand = new BitcoinCommand(_httpService);
     }
 
     [Test]
     public void Test_IsAllowedInPrivateMessage_ShouldReturnTrue()
     {
-        // Assert
         Assert.That(_bitcoinCommand.IsAllowedInPrivateMessage, Is.True);
+    }
+
+    [Test]
+    public void Test_RequiredRank_ShouldBeRegular()
+    {
+        Assert.That(_bitcoinCommand.RequiredRank, Is.EqualTo(Rank.Regular));
     }
 
     [Test]
     public async Task Test_RunAsync_ShouldReplyWithBitcoinRates_WhenApiCallSucceeds()
     {
         // Arrange
-        var mockResponse = new HttpResponse<IDictionary<string, IDictionary<string, int>>>
+        var mockResponse = new HttpResponse<IDictionary<string, IDictionary<string, double>>>
         {
-            Data = new Dictionary<string, IDictionary<string, int>>
+            Data = new Dictionary<string, IDictionary<string, double>>
             {
-                ["bitcoin"] = new Dictionary<string, int>
+                ["bitcoin"] = new Dictionary<string, double>
                 {
                     ["eur"] = 40000,
                     ["usd"] = 42000
                 }
             }
         };
-
-        _httpService.GetAsync<IDictionary<string, IDictionary<string, int>>>(Arg.Any<string>())
+        _httpService.GetAsync<IDictionary<string, IDictionary<string, double>>>(Arg.Any<string>(),
+                cancellationToken: Arg.Any<CancellationToken>())
             .Returns(mockResponse);
 
         // Act
         await _bitcoinCommand.RunAsync(_context);
 
         // Assert
-        _context.Received().Reply("1 bitcoin = 40000€ = 42000$", rankAware: true);
+        _context.Received(1).Reply("1 bitcoin = 40000€ = 42000$", rankAware: true);
     }
 
     [Test]
     public async Task Test_RunAsync_ShouldReplyWithError_WhenApiCallFails()
     {
         // Arrange
-        _httpService.GetAsync<IDictionary<string, IDictionary<string, int>>>(Arg.Any<string>())
+        _httpService.GetAsync<IDictionary<string, IDictionary<string, double>>>(Arg.Any<string>(),
+                cancellationToken: Arg.Any<CancellationToken>())
             .Throws(new Exception("API error"));
 
         // Act
