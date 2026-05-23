4e632be6616c846192c681645ca7ae9039ea932a
slimf1 <slimane.fakani.dev@gmail.com>
2026-05-23 12:55:58 +0200

    fix(core): fix some handlers not working

--- src/ElsaMina.Core/Handlers/Handler.cs
+++ src/ElsaMina.Core/Handlers/Handler.cs
@@ -4,7 +4,7 @@ public abstract class Handler : IHandler
 {
     public bool IsEnabled { get; set; } = true;
     public virtual string Identifier => GetType().FullName;
-    public virtual IReadOnlySet<string> HandledMessageTypes => (HashSet<string>)[];
+    public virtual IReadOnlySet<string> HandledMessageTypes => null;
 
     public abstract Task HandleReceivedMessageAsync(string[] parts, string roomId = null,
         CancellationToken cancellationToken = default);
