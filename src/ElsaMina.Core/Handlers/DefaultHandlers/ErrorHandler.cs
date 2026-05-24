bda10b9db797c31134e64e61a6f56d6349a9ddcb
slimf1 <slimane.fakani.dev@gmail.com>
2026-05-24 14:03:03 +0200

    feat(handlers): log popup messages received from the server

--- src/ElsaMina.Core/Handlers/DefaultHandlers/ErrorHandler.cs
+++ src/ElsaMina.Core/Handlers/DefaultHandlers/ErrorHandler.cs
@@ -4,17 +4,21 @@ namespace ElsaMina.Core.Handlers.DefaultHandlers;
 
 public class ErrorHandler : Handler
 {
-    public override IReadOnlySet<string> HandledMessageTypes => new HashSet<string> { "error" };
+    public override IReadOnlySet<string> HandledMessageTypes => new HashSet<string> { "error", "popup" };
 
     public override Task HandleReceivedMessageAsync(string[] parts, string roomId = null,
         CancellationToken cancellationToken = default)
     {
-        if (parts.Length <= 3 || parts[1] != "error")
+        switch (parts.Length)
         {
-            return Task.CompletedTask;
+            case > 3 when parts[1] == "error":
+                Log.Error("Received error message from server : {Error}", parts[2]);
+                break;
+            case > 2 when parts[1] == "popup":
+                Log.Information("Received popup message from server : {Popup}", string.Join("|", parts[2..]));
+                break;
         }
 
-        Log.Error("Received error message from server : {Error}", parts[2]);
         return Task.CompletedTask;
     }
 }
